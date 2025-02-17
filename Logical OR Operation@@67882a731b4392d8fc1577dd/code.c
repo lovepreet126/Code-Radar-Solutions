c6a0d3f41893d1b91f08d1243e02b098b3c9f855
Lovepreet <lovepreetinderpal@gmail.com>
2025-02-17 22:08:34 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include<stdio.h>
+int main9{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>0 && b<0){
+        printf("True");
+    }
+    elif(b>0 && a<0){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+}
