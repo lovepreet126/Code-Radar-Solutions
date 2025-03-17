f4ea71a3f1185e926e4fc79f60bfd913aa36011d
Lovepreet <lovepreetinderpal@gmail.com>
2025-03-17 21:06:03 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -11,7 +11,7 @@ int main(){
             printf("Vovel");
         }
     }
-    elif if(!(a>=65 && a<=90) && (a>=0 || a<=0)){
+    else if(!(a>=65 && a<=90) && (a>=0 || a<=0)){
         printf("Digit");
     }
     else{
