bece3438a4e7f03fe55fb9bf0265a916594a23a4
Lovepreet <lovepreetinderpal@gmail.com>
2025-03-18 18:09:56 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+#include<stdio.h>
+int mmian(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a%b==0){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+}
